33dac40d09f5d94c974d97c8662081e3ae47d82d
willot <vianney.willot@gmail.com>
2015-02-25 16:37:29 -0500

    wishes migration

--- db/migrate/20150224223046_create_wishes.rb
+++ db/migrate/20150224223046_create_wishes.rb
@@ -1,6 +1,9 @@
 class CreateWishes < ActiveRecord::Migration
   def change
     create_table :wishes do |t|
+    	t.string :name
+    	t.text :description
+    	t.integer :price
 
       t.timestamps null: false
     end
