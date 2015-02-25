08703381d06b6d0977b16c7e7e42314441f458f3
willot <vianney.willot@gmail.com>
2015-02-25 16:21:05 -0500

    migration list

--- db/migrate/20150224222907_create_lists.rb
+++ db/migrate/20150224222907_create_lists.rb
@@ -1,8 +1,14 @@
 class CreateLists < ActiveRecord::Migration
   def change
     create_table :lists do |t|
+    	t.string :name
+    	t.integer :range_high
+    	t.integer :range_low
+    	t.text :description
+    	t.date :date
+    	t.string :period
 
-      t.timestamps null: false
+      	t.timestamps null: false
     end
   end
 end
