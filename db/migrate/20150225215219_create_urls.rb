9a312347f3e67d5fae673d129c5d6bb6a099b828
willot <vianney.willot@gmail.com>
2015-03-27 17:36:11 -0400

    add fields

--- db/migrate/20150225215219_create_urls.rb
+++ db/migrate/20150225215219_create_urls.rb
@@ -1,8 +1,10 @@
 class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
+    	t.string :url
+    	
 
-      t.timestamps null: false
+      	t.timestamps null: false
     end
   end
 end
