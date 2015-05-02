a6b74970579265ccaacf5dcae26b344a2d656c15
willot <vianney.willot@gmail.com>
2015-05-02 16:57:20 -0400

    correct error user migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 	has_many :user_lists
 	has_many :lists, through: :user_lists
-	has_many :wishes, through: :lists
+	has_many :wishes
 	has_many :urls, through: :wishes
 	has_many :administred_list, class_name: "List", source: :admin
 end
