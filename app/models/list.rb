de5e4741128369927824a30b173b379be9bb2752
willot <vianney.willot@gmail.com>
2015-05-03 16:17:39 -0400

    modify list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,7 @@
 class List < ActiveRecord::Base
 	has_many :user_lists
 	has_many :users, through: :user_lists
-	has_many :wishes, through: :users
+	has_many :wishes #, through: :users
 	has_many :urls, through: :wishes
 	belongs_to :admin, class_name: "User", foreign_key: "user_id" 
 end
