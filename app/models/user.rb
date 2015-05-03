369004f4caeb5d2ebb002f1b862d7346c3721d75
willot <vianney.willot@gmail.com>
2015-05-03 16:49:53 -0400

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
 	has_many :wishes
 	has_many :urls, through: :wishes
 	has_many :administred_list, class_name: "List", source: :admin
+
+	validates :username, uniqueness: true, presence: true
+	validates :name, presence: true
+	validates :email, presence: true 
+	
+
+	has_secure_password
 end
