3542728ee188fe3d21c8c4a0f31e814b171ff33a
willot <vianney.willot@gmail.com>
2015-05-03 16:50:12 -0400

    add validation to wish model

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -2,4 +2,9 @@ class Wish < ActiveRecord::Base
 	has_many :urls
 	belongs_to :user
 	belongs_to :list
+
+	validates :name, presence: true
+	validates_length_of :description,
+		:maximum => 500,
+		:too_long =>"Limit is 500 characters"
 end
