38873b24e6259c863e6970eccb92a1cf6f44344b
willot <vianney.willot@gmail.com>
2015-05-04 19:03:14 -0400

    read lot of stuff on has secure password decide to add a length to password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness: true 
 	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+	validates :password, length: { minimum: 8 }
 	
 
 	has_secure_password
