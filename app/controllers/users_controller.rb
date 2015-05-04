b41d1d6506a468ebec718132a714487e18b69743
willot <vianney.willot@gmail.com>
2015-05-04 18:30:43 -0400

    start user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end	
+
+	def new
+		@users = User.new
+	end	
+
+	def show
+		@user = User.find(params[:id])
+	end	
+
+	def create 
+		@user = User.new(xxxxxx)
+		
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to @user, notice:"Account created"
+		else
+			@error ="There was an issue while Signing up"
+			render :new
+		end		
+		
+	end	
+
 end
