44ca3bd366c0099ce6ba9bfea60643ea5103f6df
willot <vianney.willot@gmail.com>
2015-05-04 17:56:53 -0400

    add uniqueness for email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 
 	validates :username, uniqueness: true, presence: true
 	validates :name, presence: true
-	validates :email, presence: true 
+	validates :email, presence: true, uniqueness: true 
 	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 	
 
