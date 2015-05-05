a8d77c20348b9de05296fe298b6b5c3eaaa2c857
willot <vianney.willot@gmail.com>
2015-05-05 14:11:38 -0400

    set session controller for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+	def new
+	end 
+
+	def create
+		user = User.find_by(username: params[:username])
+
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.user_id
+			redirect_to "/"
+		else
+			@error = "Incorrect login information. Please Try again"
+			render :new
+		end		
+	end	
+
+	def destroy
+		session.delete(:user_id)
+		redirect_to '/'
+	end	
 end
