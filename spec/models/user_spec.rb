3a1d827a8dcbbca01b8afc3dbd1b20ec8b74f7cd
willot <vianney.willot@gmail.com>
2015-05-07 18:36:03 -0400

    add more validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe User, :type => :model do
   	it {expect(user).to validate_presence_of(:username)}
   	it {expect(user).to validate_presence_of(:email)}
   end	
+
+  describe "validate uniqueness" do
+  	it {expect(user).to validate_uniqueness_of(:email)}
+  	it {expect(user).to validate_uniqueness_of(:username)}
+  end	
+
+  describe "validate lenght" do
+  	it {expect(user).to ensure_length_of(:password).is_at_least(8)}
+  end	
 end
